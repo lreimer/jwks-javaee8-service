6ce5b9ce30a9fe0f89db57cdf1a5b8e64643f4e0
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-12-13 21:48:56 +0100

    Add thumbprint ID for NULL key ID.

--- src/main/java/cloud/nativ/javaee/jwks/RSAKeyFactory.java
+++ src/main/java/cloud/nativ/javaee/jwks/RSAKeyFactory.java
@@ -1,6 +1,7 @@
 package cloud.nativ.javaee.jwks;
 
 import com.nimbusds.jose.Algorithm;
+import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
 
@@ -10,6 +11,7 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Objects;
 
 /**
  * A factory implementation to create {@link RSAKey} instances.
@@ -35,13 +37,19 @@ public class RSAKeyFactory {
             RSAPublicKey pub = (RSAPublicKey) kp.getPublic();
             RSAPrivateKey priv = (RSAPrivateKey) kp.getPrivate();
 
-            return new RSAKey.Builder(pub)
+            RSAKey.Builder builder = new RSAKey.Builder(pub)
                     .privateKey(priv)
                     .keyUse(use)
-                    .algorithm(alg)
-                    .keyID(kid)
-                    .build();
-        } catch (NoSuchAlgorithmException e) {
+                    .algorithm(alg);
+
+            if (Objects.isNull(kid)) {
+                builder = builder.keyIDFromThumbprint();
+            } else {
+                builder = builder.keyID(kid);
+            }
+
+            return builder.build();
+        } catch (NoSuchAlgorithmException | JOSEException e) {
             throw new IllegalArgumentException(e);
         }
     }
